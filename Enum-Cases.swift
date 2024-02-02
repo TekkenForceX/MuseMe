2034c75e068a955e74782e9fdd9023d71b5ad4d1
Calil Hall <Calilihall@gmail.com>
2024-02-02 14:10:54 -0500

    Enum

---
+++ Enum-Cases.swift
@@ -0,0 +1,18 @@
+//
+//  Enum-Cases.swift
+//  CreativeApp
+//
+//  Created by Calil Hall on 1/31/24.
+//
+
+import SwiftUI
+
+struct Enum_Cases: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Enum_Cases()
+}
