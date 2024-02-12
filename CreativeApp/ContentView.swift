ee9dc78d658151457c3354c206feee0c88efea62
Angelo Brown <130711987+angelob120@users.noreply.github.com>
2024-02-12 14:20:32 -0500

    awerf

--- CreativeApp/ContentView.swift
+++ CreativeApp/ContentView.swift
@@ -8,7 +8,6 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var counter = 0
     @State private var blockOption: DropDownMenuOption? = nil
     @State private var trigger = false
     
@@ -25,52 +24,36 @@ struct ContentView: View {
                         placeholder: "What is blocking you?",
                         options: DropDownMenuOption.creativeBlockOptions
                     )
-                    
-                    //            Text(blockOption?.option ?? "")
-                    
-                    //                    Button(action: {
-                    //                        print("Round Action")
-                    //                        trigger.toggle()
-                    //                    }) {
-                    //                        Text("Let My Creativity Flow")
-                    //                            .frame(width : 150, height: 150)
-                    //                            .foregroundColor(.black)
-                    //                            .background(Color.red)
-                    //                            .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
-                    //                            .sensoryFeedback(.success, trigger: trigger)
-                    //                    }
-                    
-                    
+
                     NavigationLink {
                         ActivitiesView(prompt: blockOption ?? DropDownMenuOption(option: "Lack of inspiration", enumOption: .MuseMe))
                     } label: {
                         ZStack {
                             Circle()
-                                .stroke(Color.cyan, style: StrokeStyle(lineWidth: 10))
-                                .frame(width:300)
+                                .stroke(Color.cyan, lineWidth: 10)
+                                .frame(width: 300)
                             Text("Generate Muse")
                                 .font(.largeTitle)
-                                .frame(width : 200)
+                                .frame(width: 200)
                                 .foregroundColor(.white)
-                               // .background(Color.cyan)
-                               // .clipShape(Circle())
                                 .sensoryFeedback(.success, trigger: trigger)
                         }
                     }
-                }.background(
+                }
+                .background(
                     Image("wavybg")
                         .resizable()
                         .scaledToFill()
-                        .frame(width: 420, height: 100)
-                        .blur(radius: 3))
-                    
-
+                        .blur(radius: 3)
+                )
+            }
+        }
+    }
+}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
-        ContentView()
-            .environment(\.locale, Locale(identifier: "zh-Hans"))
+        ContentView().environment(\.locale, Locale(identifier: "zh-Hans"))
     }
 }
-
