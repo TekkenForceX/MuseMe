70e474acee480c42a7696fa5b6dcec2c211eedfc
Matthew Fails <matthew.c.fails.com>
2024-04-18 14:44:11 -0400

    Changed color to white.

---
+++ CreativeApp/ThreeDButton.swift
@@ -0,0 +1,18 @@
+//
+//  ThreeDButton.swift
+//  MuseMe
+//
+//  Created by Matthew Fails on 4/16/24.
+//
+
+import SwiftUI
+
+struct ThreeDButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ThreeDButton()
+}
