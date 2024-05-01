6493331f0b80890d7d1f4d61ae049385629eafe8
Matthew Fails <matthew.c.fails.com>
2024-05-01 16:06:05 -0400

    Added New Button to Content View

--- CreativeApp/ThreeDButton.swift
+++ CreativeApp/ThreeDButton.swift
@@ -10,10 +10,6 @@ import SwiftUI
 struct ThreeD: ButtonStyle {
     func makeBody(configuration: Configuration) -> some View {
         
-        
-        
-        
-        
         ZStack {
             
             let _: CGFloat = 5
@@ -21,13 +17,13 @@ struct ThreeD: ButtonStyle {
             Image("button1")
                 .resizable()
                 .offset(y: 8)
-                .frame(width: 100, height: 100)
+                .frame(width: 200, height: 200)
             
             Image("button1")
                 .resizable()
                 .offset(y: 5)
                 .offset(y: configuration.isPressed ? 5 : 0)
-                .frame(width: 100, height: 100)
+                .frame(width: 200, height: 200)
             
             configuration.label
                 .offset(y: configuration.isPressed ? 5 : 0)
@@ -55,7 +51,7 @@ struct ThreeDButton_Previews: PreviewProvider {
                 //.blur(radius: /*@START_MENU_TOKEN@*/3.0/*@END_MENU_TOKEN@*/)
         )
         .foregroundColor(.white)
-        .frame(width: 400, height: 100)
+        .frame(width: 500, height: 100)
         .buttonStyle(ThreeD())
     }
 }
