0128d22007a76746c3feadcafcf51a90192eff08
Angelo Brown <130711987+angelob120@users.noreply.github.com>
2024-05-01 14:55:39 -0400

    cooked

---
+++ CreativeApp/CloudKitManager.swift
@@ -0,0 +1,8 @@
+//
+//  CloudKitManager.swift
+//  MuseMe
+//
+//  Created by Angelo Brown on 5/1/24.
+//
+
+import Foundation
---
+++ CreativeApp/Utilities.swift
@@ -0,0 +1,8 @@
+//
+//  Utilities.swift
+//  MuseMe
+//
+//  Created by Angelo Brown on 5/1/24.
+//
+
+import Foundation
