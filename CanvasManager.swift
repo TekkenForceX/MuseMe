0bf763f93adce1bbefecc035d25a40bfb0e19849
Matthew Fails <matthew.c.fails.com>
2024-05-10 15:31:06 -0400

    Canvas Manager and Updates to Pencil Kit

---
+++ CanvasManager.swift
@@ -0,0 +1,18 @@
+//
+//  CanvasManager.swift
+//  MuseMe
+//
+//  Created by Matthew Fails on 5/9/24.
+//
+
+import SwiftUI
+
+struct CanvasManager: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CanvasManager()
+}
